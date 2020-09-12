694679a6d71ad66108ac498cee3be253801a98cd
Hyunwoo Lee <hyunwoo086@gmail.com>
2020-09-12 14:30:28 +0900

    Update video.tsx

--- components/video.tsx
+++ components/video.tsx
@@ -58,7 +58,7 @@ export default function Video() {
 
   const handleClick = () => {
     // Get playback URL from Amazon IVS API 
-    // This is Test URL from amazon-ivs-player-web-sample project (https://github.com/aws-samples/amazon-ivs-player-web-sample)
+    // Test URL from amazon-ivs-player-web-sample project (https://github.com/aws-samples/amazon-ivs-player-web-sample)
     var PLAYBACK_URL =
       "https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8";
     player.src(PLAYBACK_URL);
